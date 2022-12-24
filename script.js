e5441b41c06c3cc6dc8566ac3edf2b80276319fe
Tony <tonyngmengkiat@gmail.com>
2022-12-24 09:54:44 +0800

    add js script

---
+++ script.js
@@ -0,0 +1,10 @@
+const item = document.getElementsByTagName  ('li');
+console.log(item[0]);
+// item[0].textContent = ''
+// item[0].textContent = 'Hello 2'
+
+// for (let i=0; i < item.length; i ++) {
+//     item[i].getElementsByClassName.backgroundColor = '#f4f4f4'
+// }
+
+
